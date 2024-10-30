936b594c865f3d7d5e6e4940a4b5db0e2c14e5aa
Douzi_UI <1844532477@qq.com>
2024-10-31 02:22:47 +0800

    Add a new button to the homepage

---
+++ src/views/TestView.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="test">
+        <h1>This is an test page</h1>
+    </div>
+</template>
+<script>
+    const cod = '1234';
+    if ('' == 0) {
+        // 他们相等！但是为什么呢？？
+    }
+    if (1 < x < 3) {
+        // x是 *任何* 值都为真！
+    }
+    const obj = { width: 10, height: 15 };
+    // 为什么是 NaN？拼写好难！
+    const area = obj.width * obj.height;
+
+    (x) => {
+        console.log(x);
+    };
+    (x) => {
+        console.log(x);
+    };
+</script>
+<style>
+    @media (min-width: 1024px) {
+        .about {
+            min-height: 100vh;
+            display: flex;
+            align-items: center;
+        }
+    }
+</style>
